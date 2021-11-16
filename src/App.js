9611ad788b5f85466bf0b999b02cce7292db8dbf
sofianenabli <sofienne.nabli@aleia.io>
2021-11-16 16:05:48 +0100

    calculator

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import { evaluate } from 'mathjs';
+import './App.css';
 
 function App() {
 
@@ -10,7 +11,11 @@ function App() {
     }
 
     const handleResult = () => {
-        setResult(evaluate(result));
+        try {
+            setResult(evaluate(result));
+        } catch {
+            setResult('Error');
+        }
     }
 
     const clear = () => {
@@ -24,28 +29,28 @@ function App() {
     return (
         <div className="container">
             <form>
-                <input type="text" value={result} />
+                <input className="result-text" type="text" value={result} />
             </form>
 
             <div className="keypad">
-                <button onClick={clear}>Clear</button>
-                <button onClick={deleteNum}>C</button>
-                <button name="/" onClick={handleButtonChange}>&divide;</button>
+                <button className="highlight" id="clear" onClick={clear}>Clear</button>
+                <button className="highlight" onClick={deleteNum}>C</button>
+                <button className="highlight" name="/" onClick={handleButtonChange}>&divide;</button>
                 <button name="7" onClick={handleButtonChange}>7</button>
                 <button name="8" onClick={handleButtonChange}>8</button>
                 <button name="9" onClick={handleButtonChange}>9</button>
-                <button name="*" onClick={handleButtonChange}>&times;</button>
+                <button className="highlight" name="*" onClick={handleButtonChange}>&times;</button>
                 <button name="4" onClick={handleButtonChange}>4</button>
                 <button name="5" onClick={handleButtonChange}>5</button>
                 <button name="6" onClick={handleButtonChange}>6</button>
-                <button name="-" onClick={handleButtonChange}>&ndash;</button>
+                <button className="highlight" name="-" onClick={handleButtonChange}>&ndash;</button>
                 <button name="1" onClick={handleButtonChange}>1</button>
                 <button name="2" onClick={handleButtonChange}>2</button>
                 <button name="3" onClick={handleButtonChange}>3</button>
-                <button name="+" onClick={handleButtonChange}>+</button>
+                <button className="highlight" name="+" onClick={handleButtonChange}>+</button>
                 <button name="0" onClick={handleButtonChange}>0</button>
                 <button name="." onClick={handleButtonChange}>.</button>
-                <button onClick={handleResult}>=</button>
+                <button className="highlight" id="result" onClick={handleResult}>=</button>
             </div>
         </div>
     )
